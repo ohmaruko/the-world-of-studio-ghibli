77f3db4c86524ef7a11a616929dbf2b1bf248ff8
madoka <mnogaki@my.bcit.ca>
2024-06-02 20:57:31 -0700

    initial commit

---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-1087.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBTabs.init( '.uagb-block-c9f37b8b' );
+	UAGBTabs.anchorTabId( '.uagb-block-c9f37b8b' );
+});
+window.addEventListener( 'hashchange', function() {
+	UAGBTabs.anchorTabId( '.uagb-block-c9f37b8b' );
+}, false );
+ });
